2a11a327836d55eb36c02aa613d40a5952bc0595
araujo.marlon@hotmail.com <araujo.marlon@hotmail.com>
2024-03-19 19:54:18 -0300

    First commit

---
+++ ProducerFiap/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProducerFiap.Controllers
+{
+	[ApiController]
+	[Route("{controller}/v1")]
+	public class UserController : ControllerBase
+	{
+		public UserController()
+		{
+		}
+	}
+}
+
